33c3642a15a79b39b56f8a02d65a425dfdfcee01
Khusniddinova <2110146@newuzbekistanuniversity.uz>
2022-10-23 22:21:14 +0500

    copy3

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -6,8 +6,8 @@ public class NamingConvention {
         int variable_one = 1;
         int variable_two = 2;
         int variable_three = 3;
-        final int variable_four = 4;
-        final int variable_five = 5;
+        final int VARIABLE_FOUR = 4;
+        final int VARIABLE_FIVE = 5;
         int variable_six = 6;
         int variable_seven = 7;
         int Variable_eight = 8;
